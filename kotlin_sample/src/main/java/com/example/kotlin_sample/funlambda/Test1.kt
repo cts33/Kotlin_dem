2e3bea43e900edf3e688773fc4427437cc4db778
chents <chentaishan@noboauto.com>
2021-10-19 16:15:30 +0800

    梳理基础语法  集合操作

---
+++ kotlin_sample/src/main/java/com/example/kotlin_sample/funlambda/Test1.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin_sample.funlambda
+
+/**
+ * @description
+ * @mail chentaishan@noboauto.com
+ * @date 2021/10/19
+ */
+
+fun main() {
+//    val repeatFun: String.(Int) -> String = { times -> this.repeat(times) }
+//    val twoParameters: (String, Int) -> String = repeatFun // OK
+//
+//    fun runTransformation(f: (String, Int) -> String): String {
+//        return f("hello", 3)
+//    }
+//    val result = runTransformation(repeatFun) // OK
+//
+//    println("result = $result")
+
+    val sourceList = mutableListOf(1, 2, 3)
+    val copySet = sourceList.toMutableSet()
+    copySet.add(3)
+    copySet.add(4)
+    println(copySet)
+}
+
